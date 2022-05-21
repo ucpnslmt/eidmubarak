2b41ea9d4bdec1e3ec0c9695e2393c547693a6de
ucpnslmt <ucpn.slmt@gmail.com>
2022-05-22 00:22:31 +0700

    Mengganti screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,16 @@ module.exports = {
         poppins: ['Poppins']
       }
     },
+    screens: {
+      'sm': '400px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
   },
   plugins: [],
 }
